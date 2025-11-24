807aaec693b180661cf4f4f3eb93fb72169d1f23
Row <axlrower@gmail.com>
2025-11-24 11:24:53 -0300

    .

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import './Login.css';
+import './login.css';
 import { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router";
