fb21c6b08219e3365b121ce951c189995dff20fc
Row <axlrower@gmail.com>
2025-11-24 11:22:33 -0300

    arreglo de sintaxis con el import login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import './Login.css'
+import './Login.css';
 import { useState, useEffect } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useNavigate } from "react-router";
